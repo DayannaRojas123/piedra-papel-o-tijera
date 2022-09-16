804d725c31e60f315699d7b3a739e38cc6eeb7fe
Jorshua Hernandez <jorshua321@gmail.com>
2022-09-16 14:20:00 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const person ={
+    name: "pepe",
+    lastname: "perez",
+    age:30,
+    country: "colombia"
+}
+
+
+const number=[30,40,50,60]
+//for in para los objetos
+
+for (let atri in person){
+    console.log(person)
+}
+
+//for of para obejetos iterables -arrays o strings
+
+for (let ele of number){
+    consolr.log (ele)
+}
