62ae2d616a76f2f384315e2bef0201b168ce0819
Dayanna Valentina Rojas <rojasdahianna123@gmail.com>
2022-09-20 15:24:31 -0500

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,86 @@
-let number=+prompt("dame un numero")
 
-for(let i=1;i<=9;i++){
-document.write (`${number}x${i}=${number*i} <br> `)
+let n=0
+ let contu=0  
+ let contpc=0
+
+let game=+prompt("digite la cantidad de veces que desea jugar")
+
+
+
+
+
+
+
+ for(let i=1;i<=game;i++){
+    let user=+prompt("seleccione una opcion : 1.piedra, 2.papel, 3.tijera")
+    let pc=Math.floor(Math.random()*3+1)
+    console.log(user)
+    console.log(pc)
+    
+    if(user===pc){
+        alert("empate")
+        contu++
+        contpc++
+    }
+
+    else if(user===1 && pc===2){
+        alert("pc gana")
+        
+        contpc++
+    }
+
+   else if(user===1 && pc===3){
+        alert("user gana")
+        contu++
+       
+    }
+
+   else if(user===2 && pc===1){
+        alert("user gana")
+        contu++
+       
+    }
+
+  
+
+    else if(user===2 && pc===3){
+        alert("pc gana")
+        contu++
+        contpc++
+    } 
+
+    else if(user===3 && pc===1){
+        alert("pc gana")
+        contu++
+        contpc++
+    } 
+
+
+    else if(user===3 && pc===2){
+        alert("user gana")
+        contu++
+        contpc++
+    } 
+
+
+
+    
 }
+
+
+
+
+if(contu>contpc){
+    alert("gana user con "+contu+"puntos"+"y pc con"+contpc+"puntos")
+}
+
+if(contpc>contu){
+    alert("gana pc "+contpc+"puntos"+"user con"+contu+"puntos")
+}
+
+if(contu===contpc){
+    alert("empate "+"user con"+contu+"puntos"+"y pc con"+contpc+"puntos")
+}
+
+
+
